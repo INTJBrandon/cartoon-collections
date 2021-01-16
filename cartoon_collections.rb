61b8e607985d84305f547e79eece5b438593a97c
seireitou <ausis@mail.com>
2021-01-16 00:02:15 -0600

    commit tree a7cd699e9669dec242d3e59745f6c31f11def6c9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do | gnomes, index |
+    puts "#{index + 1}. #{gnomes}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do | elements |
+    elements.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any?{|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  caught = []
+  while i < cheese_types.length
+  caught << array.find{|x| x == cheese_types[i]}
+  if caught[0]
+    break
+  end
+  i += 1
+  end
+    caught[0]
 end
